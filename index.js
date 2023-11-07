8eabfcf16f732bdbcfe6ad195cf8bffb6432b5a0
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-07 23:14:23 +0600

    booking colletion create and data send Successfully

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ async function run() {
     // const serviceCollection=client.db("carr").collection("services");
     const database = client.db("serviceDB");
     const serviceCollaction = database.collection("service");
+    // 
+    const databaseBooking = client.db("serviceDB");
+    const bookingCollaction = databaseBooking.collection("booking");
+
+    // user booking collection data READ
+    app.get("/booking",async(req,res)=>{
+      const result=await bookingCollaction.find().toArray();
+      res.send(result);
+      console.log(result);
+    })
+
+    // user booking collection data CREATE
+    app.post("/booking",async(req,res)=>{
+      const user=req.body;
+      console.log(user)
+      const result = await bookingCollaction.insertOne(user);
+      // const result=await serviceCollection.insertOne(user);   
+      res.send(result);
+      
+    })
+
 
 // user read
 app.get("/services",async(req,res)=>{
@@ -49,6 +70,16 @@ app.get("/services",async(req,res)=>{
     
   })
 
+  app.get('/service/:id', async(req, res) => {
+    const id=req.params.id;
+    const query={_id:new ObjectId(id)};
+    const options = {
+      projection: { title: 1, service_id: 1,price: 1,img:1 },
+    };
+
+    const result=await serviceCollaction.findOne(query,options);
+    res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
