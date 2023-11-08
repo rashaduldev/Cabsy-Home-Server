da5fb40ce2373ed1820bbc8c1c3e466b964eca21
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-08 18:11:07 +0600

    dot env and gitignore create also authentication done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port=process.env.PORT || 3000
 // user: mdrashadul898
@@ -11,8 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://mdrashadul898:xKMnvl7I8c6v68vV@cluster0.pzjduhk.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pzjduhk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -72,6 +73,7 @@ app.get("/services",async(req,res)=>{
   // user Delete
   app.delete("/services/:id",async(req,res)=>{
     const id=req.params.id;
+    console.log(id);
     const query={_id:new ObjectId(id)};
     const result = await serviceCollaction.deleteOne(query); 
     res.send(result);
@@ -85,8 +87,21 @@ app.get("/services",async(req,res)=>{
   })
   app.put("/services/:id",async(req,res)=>{
     const id=req.params.id;
-    const query={_id:new ObjectId(id)};
-    const result = await serviceCollaction.findOne(query); 
+    const filter={_id:new ObjectId(id)};
+    const options={upsert:true};
+    const updatedService=req.body;
+    const service={
+      $set:{
+        pictureURL:updatedService.pictureURL,
+        serviceName:updatedService.serviceName,
+        description:updatedService.description,
+        serviceArea:updatedService.serviceArea,
+        price:updatedService.price,
+        userName:updatedService.userName,
+        useremail:updatedService.useremail,
+      }
+    }
+    const result = await serviceCollaction.updateOne(filter,service,options); 
     res.send(result);
   })
 
